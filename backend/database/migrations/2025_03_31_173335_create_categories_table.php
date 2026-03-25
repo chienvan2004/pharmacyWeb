d2ecf45c32c0afbf1596a9b8100113ecdf5ba018
chienvan2004 <tavanchien360@gmail.com>
2026-03-25 20:16:23 +0700

    Fix categories  migration with table check

--- backend/database/migrations/2025_03_31_173335_create_categories_table.php
+++ backend/database/migrations/2025_03_31_173335_create_categories_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
+        if (!Schema::hasTable('categories')) {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('parent_id')->nullable();
@@ -26,6 +27,7 @@ return new class extends Migration
             $table->foreign('parent_id')->references('id')->on('categories')->nullOnDelete();
             $table->timestamps();
         });
+        }
     }
 
     /**
